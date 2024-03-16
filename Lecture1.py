d5a0c253c7413fdcdf7f0a429fb2b96643ac9bc6
PawelKowalcze <kowalczepawel2004@gmail.com>
2024-03-16 18:49:52 +0100

    Lecture 1

--- Lecture1.py
+++ Lecture1.py
@@ -1,3 +1,5 @@
+from array import *
+
 print("Hello World!")
 
 a = 10
@@ -31,6 +33,39 @@ print('Mark' in persons)
 print(persons * 2)
 print(persons + ['Mark'])
 
-a = [1, 2]
+a = [1, 2, 3, 5]
 print(1 in a)
 print(a.pop())
+print(a)
+print(a.pop(0))  # pop(index of element)
+print(a)
+a.append([1, 3, 5, 5, 4])
+print(a)
+print(a.count(2))
+
+ages = {'Tom': 20, 'John': 19, 'Smith': 10}
+print(ages['John'])
+
+record = {
+    'Name': 'Mark',
+    'Age': 63
+}
+
+print(record)
+print(record['Name'])
+record['Name'] = 'Lucy'
+record['Age'] = 25
+print(record)
+
+x = array('f', [1, 2, 3])
+print(x)
+print(x[0])
+print(type(x))
+
+y = int(input('Insert text: '))
+print(type(y))
+
+l = ['a', 'b', 'c', 3]
+for i in l:
+    print(i)
+
